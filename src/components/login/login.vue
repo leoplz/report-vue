bfad286d729cb8d00e60c3f305bb9fa2542470fb
unknown <782411603@qq.com>
2020-11-08 11:57:40 +0800

    first time init main

---
+++ src/components/login/login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login_container">you must login !</div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+
+<style lang="less" scoped>
+.login_container {
+  background: #2b4b6b;
+  height:100%;
+}
+</style>
